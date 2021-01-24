7a87868d0144eaacb1ec97917d308310c95a3e61
Illés Tóth <illestothvisualz@gmail.com>
2021-01-24 20:39:43 +0100

    product-screen/nodejs-server/add-redux/cart

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import data from '../data';
 import Product from '../components/Product';
-import MessageBox from '../components/MessageBox';
 import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    const dispatch = useDispatch();
+    const productList = useSelector((state) => state.productList);
+    const { loading, error, products } = productList;
 
     useEffect(() => {
-        const fetchData = async () => {
-           try {
-            setLoading(true);
-            const { data } = await axios.get('/api/products');
-            setLoading(false);
-            setProducts(data);
-           } catch (err){
-               setError(err.message);
-               setLoading(false);
-           }
-        };
-        fetchData();
-    }, []);
+        dispatch(listProducts());
+    }, [dispatch]);
     return (
         <div>
             {loading ? (
@@ -31,7 +22,7 @@ export default function HomeScreen() {
                 <MessageBox variant="danger">{error}</MessageBox>
             ) : (
                 <div className="row center">
-                {products.map((product) => (
+                {data.products.map((product) => (
                     <Product key={product._id} product={product}></Product>
             ))}
           </div>
